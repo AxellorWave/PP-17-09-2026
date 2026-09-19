9201b944269a4265087de245607e3990d67bbe58
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-09-19 22:40:13 +0300

    init functions and structure

--- main.cpp
+++ main.cpp
@@ -1,2 +1,23 @@
-int main()
+#include <iostream>
+#include <random>
+#include <pthread.h>
+#include <vector>
+
+namespace zharov
+{
+  struct Task
+  {
+    double r;
+    size_t tests;
+    size_t seed;
+    size_t inside;
+  };
+
+  double area(double r, size_t threds, size_t tests);
+  size_t calc(double r, size_t tests, size_t seed);
+  bool isInside(double x, double y, double r);
+  void* worker(void* data);
+}
+
+int main(int argc, char** argv)
 {}
