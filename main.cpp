da2d2e0b7f82920196e2e65388e17035992a4e9e
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-09-19 22:50:31 +0300

    main func ready

--- main.cpp
+++ main.cpp
@@ -20,4 +20,28 @@ namespace zharov
 }
 
 int main(int argc, char** argv)
-{}
+{
+  size_t threads = 4;
+  if (argc > 1)
+  {
+    threads = std::stoull(argv[1]);
+  }
+
+  double r;
+  size_t tests;
+  std::cin >> r >> tests;
+  if (r <= 0 || tests == 0 || threads == 0) {
+    std::cerr << "Args must be positive\n";
+    return 1;
+  }
+
+  try
+  {
+    std::cout << zharov::area(r, threads, tests) << "\n";
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+}
